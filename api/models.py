a8d113f3e86d1f3e482401a8e519bc9a5baa4aa4
Peter Peng <yser.pc@gmail.com>
2023-01-25 20:46:56 -0500

    added a delete method

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from . import db
 class Package(db.Model):
     __tablename__ = "packages"
 
-    id = db.Column(db.Integer, primary_key=True)
+    locker_id = db.Column(db.Integer, primary_key=True)
     package_id = db.Column(db.String(50))
     name = db.Column(db.String(50))
     student_id = db.Column(db.String(20))
--- api/views.py
+++ api/views.py
@@ -5,10 +5,11 @@ from .models import Package
 main = Blueprint('main', __name__)
 
 
-@main.route('/add_package', methods=['POST'])
+@main.route('/new', methods=['POST'])
 def add_package():
     package_data = request.get_json()
     new_package = Package(
+        locker_id=package_data['locker_id'],
         package_id=package_data['package_id'],
         name=package_data['name'],
         student_id=package_data['student_id'],
@@ -20,17 +21,28 @@ def add_package():
     return 'Done', 201
 
 
-@main.route('/packages')
+@main.route('/packages', methods=['GET'])
 def packages():
     package_list = Package.query.all()
     packages = []
 
     for package in package_list:
         packages.append({
+            'locker_id': package.locker_id,
             'package_id': package.package_id,
             'name': package.name,
             'student_id': package.student_id,
             'email': package.email
         })
 
-    return jsonify({'packages': packages})
+    return jsonify({'packages': packages}), 200
+
+
+@main.route('/delete', methods=["DELETE"])
+def update_package():
+    package_data = request.get_json()
+    package = db.get_or_404(Package, package_data["locker_id"])
+    db.session.delete(package)
+    db.session.commit()
+
+    return 'Done', 201
