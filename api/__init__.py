a87e5acd4c96329dcdf478a730d49112ebfb448e
Peter Peng <yser.pc@gmail.com>
2023-02-20 09:53:26 -0500

    use environ for secret key

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+import os
 
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thisissecret'
+    app.config['SECRET_KEY'] = os.environ['LOCKER_SECRET_KEY']
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     db.init_app(app)
 
