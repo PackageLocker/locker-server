20c5810249da56cc53f2c8bfc0decf29aa785419
Sawyer Travis <sawtravis@gmail.com>
2023-03-17 16:38:59 -0400

    HTML email addresses the reciever by name

--- api/notification.py
+++ api/notification.py
@@ -3,8 +3,9 @@ from email.message import EmailMessage
 import ssl, smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
 
-def notification(email_receiver):
+def notification(email_receiver, email_name):
     # Login Information
     email_sender = 'teamknightpickup@gmail.com'
     email_password = os.environ.get('EMAIL_KEY')
@@ -14,6 +15,17 @@ def notification(email_receiver):
     email_message['From'] = email_sender
     email_message['To'] = email_receiver
     email_message['Subject'] = 'Package Ready for Pickup'
+
+    # Customize HTML Email
+    custom_string = "Hello " + email_name + ", your package is ready for pickup. Please proceed to the dorm lobby to pick up your package."
+    with open("api/email.html") as html_file:
+        soup = BeautifulSoup(html_file, "html.parser")
+        name_element = soup.find("p", class_="para", id="name")
+        name_element.string = custom_string
+        html_file.close()
+    with open("api/email.html", "w") as html_file:
+        html_file.write(str(soup))
+        html_file.close()
 
     # HTML Email Body
     html = open('api/email.html')
